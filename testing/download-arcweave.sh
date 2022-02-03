c4969efc478d11e447475a3faa401dc1ad6cc090
Lenard Frommelt <lenny09918050@gmail.com>
2022-02-03 18:21:37 +0100

    fixed wget script

--- testing/download-arcweave.sh
+++ testing/download-arcweave.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget --mirror --convert-links --page-requisites -P local-copies arcweave-wget
+wget --mirror --convert-links --page-requisites -P local-copies/arcweave-wget arcweave.com
